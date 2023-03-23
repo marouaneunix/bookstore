6a77966a2e12aaba2b7f50360de48372d9b8bf9b
Mostareasma <56792517+Mostareasma@users.noreply.github.com>
2023-03-23 15:13:35 +0000

    [Feat] created category controller

--- src/main/java/ma/norsys/bookstore/controllers/CategoryController.java
+++ src/main/java/ma/norsys/bookstore/controllers/CategoryController.java
@@ -1,2 +1,26 @@
-package ma.norsys.bookstore.controllers;public class CategoryController {
+package ma.norsys.bookstore.controllers;
+
+
+import ma.norsys.bookstore.models.Category;
+import ma.norsys.bookstore.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll() {
+        List<Category> categories= categoryRepository.findAll();
+        return ResponseEntity.ok().body(categories);
+    }
+
+
 }
