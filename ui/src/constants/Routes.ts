7e3974e71afb8be707a7dad632de21dfc2fd4555
Mostareasma <56792517+Mostareasma@users.noreply.github.com>
2023-03-24 11:21:46 +0000

    [Feat] Added home page

--- ui/src/constants/Routes.ts
+++ ui/src/constants/Routes.ts
@@ -1,9 +1,14 @@
 import {BooksPage} from "../pages/BooksPage.js";
 import CreateBookPage from "../pages/CreateBookPage.js";
+import Home from "../pages/Home.js";
 import ShowBook from "../pages/ShowBook.js";
 
 
 export const APP_ROUTES = [
+    {
+        path: '/',
+        element: Home
+    },
     {
         path: '/books',
         element: BooksPage
