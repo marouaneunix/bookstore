8b33c57c2f5e5f80e8a215807b0ca347e956ca19
ETTAMLI-Kawthar <kawtharettamli@gmail.com>
2023-03-25 21:16:35 +0000

    [FIX] Book page

--- ui/src/pages/BooksPage.tsx
+++ ui/src/pages/BooksPage.tsx
@@ -72,7 +72,7 @@ export const BooksPage = () => {
                         required
                      />
                            
-                    <button onClick={postData} type="submit" className="text-white absolute right-2 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    <button onClick={postData} type="submit" className="text-white absolute center bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
             </form>
             <div>
